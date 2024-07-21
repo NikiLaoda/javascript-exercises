6479041a16ed5d65023fbe8a4f933897e37c53ac
Nikita Pustovalov <workpagelaoda@gmail.com>
2024-07-21 17:57:04 +0300

    Test 3 done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let newString = [];
+    for (let i = 0; i <= string.length; i++) newString[i] = string.at(string.length - i);
+    return newString.join('');
 };
 
 // Do not edit below this line
