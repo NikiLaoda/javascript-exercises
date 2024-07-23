e463edb0572b2bb40f242f58bf49740dc4e16555
Nikita Pustovalov <workpagelaoda@gmail.com>
2024-07-23 17:46:50 +0300

    Test 8 done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+  let summary =  a.reduce((sum, CSSMathValue) => sum + CSSMathValue, 0,);
+  return summary;
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  let multiplies = a.reduce((sum, caches) => sum * caches, 1,);
+  return multiplies;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let factorial = 1;
+  for (let i = 1; i <= a; i++) factorial *= i;
+  return factorial;
 };
 
 // Do not edit below this line
