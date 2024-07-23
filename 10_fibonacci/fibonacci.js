64d38dad89ad58ea4b961eb895484189423dc4aa
Nikita Pustovalov <workpagelaoda@gmail.com>
2024-07-23 18:11:29 +0300

    Test 10 done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) return "OOPS";
+    else {
+        let array = [0, 1];
+        for (let i = 1; i <= 1000; i++) array.push(array[i] + array[i - 1]);
+        return array[num];
+    }
 };
 
 // Do not edit below this line
